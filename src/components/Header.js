ca229546e7c7f68e74aee10c4b8b63c2d96c0f52
Roushan Kumar <vk5003930@gmail.com>
2023-09-11 13:24:20 +0530

    Add initial project files and components

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { 
+  GAME_STATE_DRAW,
+  GAME_STATE_PLAYING,
+  GAME_STATE_WIN
+  } from "../Constants";
+
+const Header = ({gameState, currentPlayer, winPlayer}) => {
+  const renderLable = () => {
+    switch(gameState){
+      case GAME_STATE_PLAYING:
+      return <div> player {currentPlayer} Turn </div>
+      case GAME_STATE_WIN:
+        return <div> Player{winPlayer} Wins </div>
+      case GAME_STATE_DRAW:
+      return <div> Game is a Draw! </div>
+        default:
+    }
+  }
+  return (
+    <div className="panel header">
+        <div className="header-text"> {renderLable()} </div>
+    </div >
+  );
+};
+
+export default Header;
